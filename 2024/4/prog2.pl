e9b6f83609bb847acc0fac2d192aef9ae25bad24
RobotSigmund <ss@robotnorge.no>
2024-12-04 19:04:35 +0100

    Create prog2.pl

---
+++ 2024/4/prog2.pl
@@ -0,0 +1,53 @@
+
+use strict;
+
+
+
+# Read input into $input
+open(my $IN, '<input.txt');
+read($IN, my $input, -s $IN);
+close($IN);
+
+# Open output file for logging and data verification
+open(my $FILE, '>output.txt');
+
+# Split data up by lines
+my(@lines) = split(/[\r\n]/, $input);
+
+# Loop through each do() section, each $enabled will then contain "<data>don't()<data>".
+my $count = 0;
+foreach my $y (0..$#lines) {
+	foreach my $x (0..length($lines[0])) {
+		$count++ if (
+			(((letter($x, $y, @lines) eq 'M') && (letter(($x + 1), ($y + 1), @lines) eq 'A') && (letter(($x + 2), ($y + 2), @lines) eq 'S'))
+			|| ((letter($x, $y, @lines) eq 'S') && (letter(($x + 1), ($y + 1), @lines) eq 'A') && (letter(($x + 2), ($y + 2), @lines) eq 'M')))
+			&& (((letter(($x + 2), $y, @lines) eq 'M') && (letter(($x + 1), ($y + 1), @lines) eq 'A') && (letter($x, ($y + 2), @lines) eq 'S'))
+			|| ((letter(($x + 2), $y, @lines) eq 'S') && (letter(($x + 1), ($y + 1), @lines) eq 'A') && (letter($x, ($y + 2), @lines) eq 'M')))
+			);
+	}
+}
+
+print $FILE 'Output: ['.$count.']'."\n";
+
+# Close outfile
+close($FILE);
+
+
+
+exit;
+
+
+
+sub letter {
+	my($x, $y, @data) = @_;
+	
+	# Return junk if out-of-bounds
+	return '-' if ($x > length($data[$y]));
+	return '-' if ($y > $#data);
+	
+	return substr($data[$y],$x,1);	
+}
+
+
+
+
