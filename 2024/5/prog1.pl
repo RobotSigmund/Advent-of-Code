062ec7c161ec9cd398b459545d9583f769eb7888
Sigmund Straumland <45029038+RobotSigmund@users.noreply.github.com>
2024-12-05 15:51:33 +0100

    Update prog1.pl

--- 2024/5/prog1.pl
+++ 2024/5/prog1.pl
@@ -16,18 +16,24 @@ my($rules, $orders) = split(/[\r\n]{2}/, $input);
 my(@rulesets) = split(/[\r\n]/, $rules);
 my(@printorders) = split(/[\r\n]/, $orders);
 
-# Find valid printorders
 my $sum = 0;
+
+# Loop through printorders
 foreach my $printorder (@printorders) {
-	my @values = split(/,/, $printorder);
-	my $failedrule = CheckValid($printorder, @rulesets);
-	if ($failedrule == 0) {
-		my $middlevalue = $values[int($#values / 2)];
-		$sum += $middlevalue;
-		print $FILE $printorder.', OK, Middlevalue= '.$middlevalue.' AccSum='.$sum."\n";
-	} else {
+
+	# Check printorder, 0= success, otherwise we get failing rule
+	if (my $failedrule = CheckValid($printorder, @rulesets)) {
+		# Failed
 		print $FILE $printorder.', failed by rule '.$failedrule."\n";
+		next;
 	}
+
+	# Order is ok
+	my @values = split(/,/, $printorder);
+	my $middlevalue = $values[int($#values / 2)];
+	$sum += $middlevalue;
+	print $FILE $printorder.', OK, Middlevalue= '.$middlevalue.' AccSum='.$sum."\n";
+	
 }
 
 print $FILE 'Output: ['.$sum.']'."\n";
@@ -46,13 +52,10 @@ sub CheckValid {
 	
 	foreach my $rule (@rulesets) {
 		my($val1, $val2) = $rule =~ /(\d+)\|(\d+)/;
-		if ($printorder =~ /$val2\D+$val1/) {
-			# Wrong order, so return failed
-			return $rule;
-		} else {
-			# ok, next
-		}
+		# If wrong order, return failing rule
+		return $rule if ($printorder =~ /$val2\D+$val1/);
 	}
-	
+
+	# All rules passed so return 0 for success	
 	return 0;
 }
